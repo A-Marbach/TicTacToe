552745efb625b8a75ba841e10ade0e8be195d7aa
Artur Marbach <Marbach.a@gmx.net>
2022-10-16 23:20:43 +0200

    spiel fertig ausser unentschieden screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let fields = [];
 let gameOver = false;
 let currentShape = 'cross';
+let AUDIO_SUCCESS = new Audio('sounds/success.mp3');
 
 function fillShape(id) {
     if (!fields[id] && !gameOver) {
@@ -8,19 +9,19 @@ function fillShape(id) {
             currentShape = 'circle';
             document.getElementById('player-1').classList.add('player-inactive');
             document.getElementById('player-2').classList.remove('player-inactive');
-
         } else {
             currentShape = 'cross';
-            document.getElementById('player-1').classList.add('player-inactive');
-            document.getElementById('player-2').classList.remove('player-inactive');
+            document.getElementById('player-1').classList.remove('player-inactive');
+            document.getElementById('player-2').classList.add('player-inactive');
         }
-
         fields[id] = currentShape;
         console.log(fields);
         draw();
         checkForWin();
     }
 }
+
+
 function draw() {
     for (let i = 0; i < fields.length; i++) {
         if (fields[i] == 'circle') {
@@ -32,73 +33,97 @@ function draw() {
     }
 }
 
-function checkForWin() {
 
+function checkForWin() {
+    let mediaQuery = window.matchMedia("(max-width: 490px)");
     let winner;
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
-        document.getElementById('line-0').style.transform = 'scaleX(1)';
+        if (mediaQuery.matches) {
+            document.getElementById('line-0').style.transform = 'scaleX(0.8)';
+        } else {
+            document.getElementById('line-0').style.transform = 'scaleX(1)';
+        }
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
-        document.getElementById('line-1').style.transform = 'scaleX(1)';
-
+        if (mediaQuery.matches) {
+            document.getElementById('line-1').style.transform = 'scaleX(0.8)';
+        } else {
+            document.getElementById('line-1').style.transform = 'scaleX(1)';
+        }
     }
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        if (mediaQuery.matches){
+            document.getElementById('line-2').style.transform = 'scaleX(0.8)';
+            }else{
         document.getElementById('line-2').style.transform = 'scaleX(1)';
-
+            }
     }
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        if (mediaQuery.matches){
+            document.getElementById('line-3').style.transform = ' rotate(90deg)scaleX(0.8)';
+            }else{
         document.getElementById('line-3').style.transform = ' rotate(90deg) scaleX(1)';
-
+            }
     }
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        if (mediaQuery.matches){
+            document.getElementById('line-6').style.transform = ' rotate(45deg)scaleX(0.9)';
+            }else{
         document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1.2)';
-
+            }
     }
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        if (mediaQuery.matches){
+            document.getElementById('line-4').style.transform = ' rotate(90deg)scaleX(0.8)';
+            }else{
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
-
+            }
     }
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
+        if (mediaQuery.matches){
+            document.getElementById('line-5').style.transform = ' rotate(90deg)scaleX(0.8)';
+            }else{
         document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
-
+            }
     }
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        if (mediaQuery.matches){
+            document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(0.9)';
+            }else{
         document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1.2)';
-
+            }
     }
     if (winner) {
         console.log('Gewonnen', winner)
         gameOver = true;
+        
         setTimeout(function () {
             document.getElementById('game-over').classList.remove('d-none');
-        }, 1000);
-        setTimeout(function () {
             document.getElementById('new-game-btn').classList.remove('d-none');
-        }, 1500);
-
+            for (let i = 0; i < 9; i++) {
+                document.getElementById(`circle-` + i).classList.add('d-none');
+                document.getElementById(`cross-` + i).classList.add('d-none');
+            }
+            for (i = 0; i < 8; i++) {
+                document.getElementById(`line-` + i).style.transform = 'scaleX(0.0)';
+            }
+            AUDIO_SUCCESS.play();
+        }, 1000);
     }
 }
 
-function restart(){
+
+function restart() {
     gameOver = false;
     fields = [];
     document.getElementById('game-over').classList.add('d-none');
     document.getElementById('new-game-btn').classList.add('d-none');
-    for(i=0; i < 8; i++){
-        document.getElementById(`line-` + i).classList.add('d-none');
-    }
-    for (let i = 0; i < 9; i++){
-        document.getElementById(`circle-` + i).classList.add('d-none');
-        document.getElementById(`cross-` + i).classList.add('d-none');
-        
-    }
-
 }
